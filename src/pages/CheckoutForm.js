5bbfab8fba1d30b4e7ca5c4bb4b40844048f4469
ViruPatel3011 <pviral3011@gmail.com>
2024-09-19 11:34:45 +0530

    wip: Stripe Webhook Integration

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -1,19 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     PaymentElement,
     useStripe,
     useElements
 } from "@stripe/react-stripe-js";
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 import { selectCurrentOrder } from "../features/orders/orderSlice";
 
-export default function CheckoutForm({ dpmCheckerLink }) {
+export default function CheckoutForm() {
     const stripe = useStripe();
     const elements = useElements();
-    const currentOrder=useSelector(selectCurrentOrder);
+    const currentOrder = useSelector(selectCurrentOrder);
 
     const [message, setMessage] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (!stripe) {
+            return;
+        }
+
+        const clientSecret = new URLSearchParams(window.location.search).get(
+            "payment_intent_client_secret"
+        );
+
+        if (!clientSecret) {
+            return;
+        }
+
+        stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+            switch (paymentIntent.status) {
+                case "succeeded":
+                    setMessage("Payment succeeded!");
+                    break;
+                case "processing":
+                    setMessage("Your payment is processing.");
+                    break;
+                case "requires_payment_method":
+                    setMessage("Your payment was not successful, please try again.");
+                    break;
+                default:
+                    setMessage("Something went wrong.");
+                    break;
+            }
+        });
+    }, [stripe]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -30,11 +61,15 @@ export default function CheckoutForm({ dpmCheckerLink }) {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: `/order-success/${currentOrder?.id}`,
+                return_url: `http://localhost:3000/order-success/${currentOrder.id}`,
             },
         });
 
-       
+        // This point will only be reached if there is an immediate error when
+        // confirming the payment. Otherwise, your customer will be redirected to
+        // your `return_url`. For some payment methods like iDEAL, your customer will
+        // be redirected to an intermediate site first to authorize the payment, then
+        // redirected to the `return_url`.
         if (error.type === "card_error" || error.type === "validation_error") {
             setMessage(error.message);
         } else {
@@ -49,24 +84,15 @@ export default function CheckoutForm({ dpmCheckerLink }) {
     }
 
     return (
-        <>
-            <form id="payment-form" onSubmit={handleSubmit}>
-                <PaymentElement id="payment-element" options={paymentElementOptions} />
-                <button disabled={isLoading || !stripe || !elements} id="submit">
-                    <span id="button-text">
-                        {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
-                    </span>
-                </button>
-                {/* Show any error or success messages */}
-                {message && <div id="payment-message">{message}</div>}
-            </form>
-
-            <div id="dpm-annotation">
-                <p>
-                    Payment methods are dynamically displayed based on customer location, order amount, and currency.&nbsp;
-                    <a href={dpmCheckerLink} target="_blank" rel="noopener noreferrer" id="dpm-integration-checker">Preview payment methods by transaction</a>
-                </p>
-            </div>
-        </>
+        <form id="payment-form" onSubmit={handleSubmit}>
+            <PaymentElement id="payment-element" options={paymentElementOptions} />
+            <button disabled={isLoading || !stripe || !elements} id="submit">
+                <span id="button-text">
+                    {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+                </span>
+            </button>
+            {/* Show any error or success messages */}
+            {message && <div id="payment-message">{message}</div>}
+        </form>
     );
 }
--- src/pages/StripeCheckOut.js
+++ src/pages/StripeCheckOut.js
@@ -1,25 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-} from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 import CheckoutForm from "./CheckoutForm";
-import CompletePage from "./CompletePage";
 import "../Stripe.css";
-import { useSelector } from "react-redux";
 import { selectCurrentOrder } from "../features/orders/orderSlice";
 
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe("pk_test_51Q0bXuDsLIm1HpbYyOIalkjloBLEkdr4oslromYw5hv2OwuwTf8AKPPyzbKXCslUUCcRakUaolrpEw4coUftfB9n00ntlf6G3G");
 
-// Sign in to see your own test API key embedded in code samples.
-const stripePromise = loadStripe("pk_test_51BTUDGJAJfZb9HEBwDg86TN1KNprHjkfipXmEDMb0gSCassK5T3ZfxsAbcgKVmAIXF7oZ6ItlZZbXO6idTHE67IM007EwQ4uN3");
-
-export default function StripeCheckOut() {
+export default function StripeCheckout() {
     const [clientSecret, setClientSecret] = useState("");
-    const [dpmCheckerLink, setDpmCheckerLink] = useState("");
     const currentOrder = useSelector(selectCurrentOrder)
 
     useEffect(() => {
@@ -27,14 +20,15 @@ export default function StripeCheckOut() {
         fetch("http://localhost:8080/create-payment-intent", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ items: [currentOrder?.items] }),
+            body: JSON.stringify({ totalAmount: currentOrder?.totalAmount }),
+            meta: {
+                orderId: currentOrder.id
+                // this info will go to stripe => and then to our webhook
+                // so we can conclude that payment was successful, even if client closes window after pay
+            }
         })
             .then((res) => res.json())
-            .then((data) => {
-                setClientSecret(data.clientSecret);
-                // [DEV] For demo purposes only
-                setDpmCheckerLink(data.dpmCheckerLink);
-            });
+            .then((data) => setClientSecret(data.clientSecret));
     }, []);
 
     const appearance = {
@@ -49,7 +43,7 @@ export default function StripeCheckOut() {
         <div className="Stripe">
             {clientSecret && (
                 <Elements options={options} stripe={stripePromise}>
-                    <CheckoutForm></CheckoutForm>
+                    <CheckoutForm />
                 </Elements>
             )}
         </div>
